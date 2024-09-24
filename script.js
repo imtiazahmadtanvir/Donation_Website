ca0dcafcd05db2edf7543fd1705a2242ed8bba7d
Imtiaz Ahmad Tanvir <imtiaztanveer07@gmail.com>
2024-09-24 20:06:23 +0600

    add congrats model

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-function openModal() {
-    document.getElementById('modal').classList.remove('hidden');
+// function openModal() {
+//     document.getElementById('modal').classList.remove('hidden');
+// }
+
+// function closeModal() {
+//     document.getElementById('modal').classList.add('hidden');
+// }
+
+function loadHistory(){
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('donanation-section').classList.add('hidden');
+}
+function loadDonation(){
+    document.getElementById('donanation-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
 }
 
-// Function to close the modal
-function closeModal() {
-    document.getElementById('modal').classList.add('hidden');
-}
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
 
+    function setActive(button) {
+      button1.classList.remove('bg-btn-color1');
+      button2.classList.remove('bg-btn-color1');
+      button1.classList.add('bg-gray-200');
+      button2.classList.add('bg-gray-200');
+      
+      button.classList.remove('bg-gray-200');
+      button.classList.add('bg-btn-color1');
+    }
+
+    button1.addEventListener('click', function() {
+      setActive(button1);
+    });
+
+    button2.addEventListener('click', function() {
+      setActive(button2);
+    });
 
 
 let totalAmount= parseFloat(document.getElementById('toatalAmount').innerText)
@@ -23,12 +51,15 @@ let qInput=parseFloat( document.getElementById('qInput').value)
 
 ndonationBtn.addEventListener('click',function(){
     let nInput= parseFloat(document.getElementById('nInput').value)
-    if (!isNaN(nInput) && nInput > 0) {
+    if (!isNaN(nInput) && nInput > 0 && nInput < totalAmount) {
         noakhaliDonation += nInput;
         document.getElementById('noakhaliDonation').innerText = noakhaliDonation;
         
         totalAmount -= nInput;
         document.getElementById('toatalAmount').innerText = totalAmount;
+
+        const ShowModal = document.getElementById('my_modal_1');
+        ShowModal.showModal();
         
      
     } else {
@@ -40,12 +71,14 @@ ndonationBtn.addEventListener('click',function(){
 
 fdonationBtn.addEventListener('click',function(){
     let fInput= parseFloat(document.getElementById('fInput').value)
-    if (!isNaN(fInput) && fInput > 0) {
+    if (!isNaN(fInput) && fInput > 0 && fInput < totalAmount) {
         feniDonation += fInput;
         document.getElementById('feniDonation').innerText = feniDonation;
         
         totalAmount -= fInput;
         document.getElementById('toatalAmount').innerText = totalAmount;
+        const ShowModal = document.getElementById('my_modal_1');
+        ShowModal.showModal();
         
       
     } else {
@@ -59,38 +92,21 @@ fdonationBtn.addEventListener('click',function(){
 
 qdonationBtn.addEventListener('click',function(){
     let qInput= parseFloat(document.getElementById('qInput').value)
-    if (!isNaN(qInput) && qInput > 0) {
+    if (!isNaN(qInput) && qInput > 0 && qInput < totalAmount ) {
         quotaDonation += qInput;
         document.getElementById('quotaDonation').innerText = quotaDonation;
         
         totalAmount -= qInput;
         document.getElementById('toatalAmount').innerText = totalAmount;
+        const ShowModal = document.getElementById('my_modal_1');
+        ShowModal.showModal();
         
       
     } else {
-        alert("Try again ")
+        alert("Try again  ")
     }
  
    
 })
 
-
-
-// history Section
-
-
-const dBtn = document.getElementById('donation-btn');
-const hBtn = document.getElementById('history-Btn');
-
-
-
-hBtn.addEventListener('click', function () {
-    hBtn.classList.add('bg-btn-color1');
-    hBtn.classList.remove('bg-gray-200');
-    dBtn.classList.remove('bg-gray-200');
-    dBtn.classList.add('bg-btn-color1');
-    
-    document.getElementById('donanation-section').classList.add('hidden');
-    document.getElementById('History-section').classList.remove('hidden');
-})
 
